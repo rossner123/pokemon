c1bc8fc997b1a3eae38a5adbc1762436c056de82
rossner123 <brumassotti@gmail.com>
2024-07-02 12:29:41 -0300

    upei os pokemon

---
+++ Pokemons/pokemons.js
@@ -0,0 +1,14 @@
+function clicou(){
+    let select = document.getElementById('botaoselect')
+    location.href = 'selecionou.html'
+}
+
+function batalha(){
+    let batalhar = document.getElementById('botaobatalha')
+    location.href = 'batalha.html'
+}
+
+function voltar(){
+    let voltar = document.getElementById('botaovoltar')
+    location.href = 'pokemons.html'
+}
